36be8d7283d9a1e0cbfa4c1a34e7d9a43f53ebac
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2022-01-04 12:58:58 -0800

    shorten int model names for postgres

--- models/shopify_holistic_reporting__customer_enhanced.sql
+++ models/shopify_holistic_reporting__customer_enhanced.sql
@@ -1,12 +1,12 @@
 with shopify_customers as (
 
     select *
-    from {{ ref('int_shopify_holistic_reporting__shopify_customer_rollup') }}
+    from {{ ref('int__shopify_customer_rollup') }}
 
 ), klaviyo_persons as (
 
     select *
-    from {{ ref('int_shopify_holistic_reporting__klaviyo_person_rollup') }}
+    from {{ ref('int__klaviyo_person_rollup') }}
 
 ), combine_customer_info as (
 
@@ -25,13 +25,13 @@ with shopify_customers as (
         klaviyo_persons.last_updated_at as klaviyo_person_last_updated_at,
         shopify_customers.is_verified_email as is_shopify_email_verified,
 
-        {{ dbt_utils.star(from=ref('int_shopify_holistic_reporting__shopify_customer_rollup'), relation_alias='shopify_customers', prefix='shopify_',
+        {{ dbt_utils.star(from=ref('int__shopify_customer_rollup'), relation_alias='shopify_customers', prefix='shopify_',
                                 except=['source_relation','email', 'full_name', 'customer_ids', 'phone_numbers', 'first_shopify_account_made_at','last_shopify_account_made_at', 
                                         'last_updated_at', 'is_verified_email'] ) 
         }},
         shopify_customers.source_relation as shopify_source_relation,
 
-        {{ dbt_utils.star(from=ref('int_shopify_holistic_reporting__klaviyo_person_rollup'), relation_alias='klaviyo_persons', prefix='klaviyo_',
+        {{ dbt_utils.star(from=ref('int__klaviyo_person_rollup'), relation_alias='klaviyo_persons', prefix='klaviyo_',
                                 except=['source_relation','email', 'full_name', 'first_klaviyo_account_made_at', 'last_klaviyo_account_made_at', 'person_ids', 'phone_numbers', 'last_updated_at'] ) 
         }},
         klaviyo_persons.source_relation as klaviyo_source_relation
--- models/shopify_holistic_reporting__daily_customer_campaign_flow.sql
+++ models/shopify_holistic_reporting__daily_customer_campaign_flow.sql
@@ -1,12 +1,12 @@
 with shopify_daily as (
 
     select *
-    from {{ ref('int_shopify_holistic_reporting__daily_shopify_customer_orders') }}
+    from {{ ref('int__daily_shopify_customer_orders') }}
 
 ), klaviyo_daily as (
 
     select *
-    from {{ ref('int_shopify_holistic_reporting__daily_klaviyo_user_campaign_flow') }}
+    from {{ ref('int__daily_klaviyo_user_campaign_flow') }}
 
 ), combine_histories as (
 
@@ -23,11 +23,11 @@ with shopify_daily as (
         coalesce(shopify_daily.last_touch_campaign_subject_line, klaviyo_daily.campaign_subject_line) as campaign_subject_line,
         coalesce(shopify_daily.last_touch_campaign_type, klaviyo_daily.campaign_type) as campaign_type,
         
-        {{ dbt_utils.star(from=ref('int_shopify_holistic_reporting__daily_shopify_customer_orders'), relation_alias='shopify_daily', prefix='shopify_',
+        {{ dbt_utils.star(from=ref('int__daily_shopify_customer_orders'), relation_alias='shopify_daily', prefix='shopify_',
                                 except=['source_relation','date_day', 'email', 'last_touch_variation_id', 'last_touch_flow_name', 'last_touch_campaign_name', 'last_touch_flow_id', 'last_touch_campaign_id', 'last_touch_campaign_subject_line', 'last_touch_campaign_type']) }},
         shopify_daily.source_relation as shopify_source_relation,
 
-        {{ dbt_utils.star(from=ref('int_shopify_holistic_reporting__daily_klaviyo_user_campaign_flow'), relation_alias='klaviyo_daily', prefix='klaviyo_',
+        {{ dbt_utils.star(from=ref('int__daily_klaviyo_user_campaign_flow'), relation_alias='klaviyo_daily', prefix='klaviyo_',
                                 except=['source_relation','date_day', 'email', 'variation_id', 'flow_name', 'campaign_name', 'last_touch_flow_id', 'last_touch_campaign_id', 'campaign_subject_line', 'campaign_type']) }},
         klaviyo_daily.source_relation as klaviyo_source_relation
 
