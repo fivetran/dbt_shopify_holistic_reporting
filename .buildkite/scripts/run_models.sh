6550a3698fbebee301194fee4800f42471e9cd8f
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2025-12-29 12:24:54 -0500

    polish

--- .buildkite/scripts/run_models.sh
+++ .buildkite/scripts/run_models.sh
@@ -21,8 +21,9 @@ dbt source freshness --target "$db" || echo "...Only verifying freshness runs…
 dbt run --target "$db" --full-refresh
 dbt run --target "$db"
 dbt test --target "$db"
-dbt run --vars '{shopify_timezone: "America/New_York", shopify_using_fulfillment_event: true, shopify_using_all_metafields: true}' --target "$db" --full-refresh
-dbt run --vars '{shopify_timezone: "America/New_York", shopify_using_fulfillment_event: true, shopify_using_all_metafields: true}' --target "$db"
-dbt run --vars '{shopify_timezone: "America/New_York", shopify_using_fulfillment_event: true, shopify_gql_using_fulfillment_event: true, shopify_using_all_metafields: true, shopify__calendar_start_date: '2020-01-01', shopify_using_abandoned_checkout: false, shopify_gql_using_abandoned_checkout: false, shopify_using_metafield: false, shopify_gql_using_metafield: false, shopify_using_discount_code_app: true, shopify_gql_using_discount_code_app: true, shopify_using_product_variant_media: true, shopify_gql_product_variant_media: true, shopify_gql_using_collection_rule: true, shopify_gql_using_customer_visit: false}' --target "$db" --full-refresh
+dbt run --vars '{shopify_timezone: "America/New_York", shopify__calendar_start_date: '2020-01-01', shopify_using_fulfillment_event: true, shopify_using_metafield: true, shopify_using_discount_code_app: true, shopify_using_product_variant_media: true}' --target "$db" --full-refresh
+dbt run --vars '{shopify_timezone: "America/New_York", shopify__calendar_start_date: '2020-01-01', shopify_using_fulfillment_event: true, shopify_using_metafield: true, shopify_using_discount_code_app: true, shopify_using_product_variant_media: true}' --target "$db"
+dbt run --vars '{shopify_api: "graphql", shopify_timezone: "America/New_York", shopify__calendar_start_date: '2020-01-01', shopify_gql_using_collection_rule: true, shopify_gql_using_fulfillment_event: true, shopify_gql_using_discount_code_app: true, shopify_gql_using_fulfillment_tracking_info: true, shopify_gql_using_product_variant_media: true}' --target "$db" --full-refresh
+dbt run --vars '{shopify_api: "graphql", shopify_timezone: "America/New_York", shopify__calendar_start_date: '2020-01-01', shopify_gql_using_collection_rule: true, shopify_gql_using_fulfillment_event: true, shopify_gql_using_discount_code_app: true, shopify_gql_using_fulfillment_tracking_info: true, shopify_gql_using_product_variant_media: true}' --target "$db"
 dbt test --target "$db"
 dbt run-operation fivetran_utils.drop_schemas_automation --target "$db"
